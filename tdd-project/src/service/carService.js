d527330d61859c4b4ac305ceb3b5199d2a94cc0d
Medina <joaopedromedinadeandrade@gmail.com>
2021-09-17 00:17:00 -0300

    finally ending tests

--- tdd-project/src/service/carService.js
+++ tdd-project/src/service/carService.js
@@ -1,28 +1,61 @@
-const BaseRepository = require('../repository/base/baseRepository')
-
-class CarService{
-  constructor({cars}){
-    this.carRepository = new BaseRepository({file:cars})
+const BaseRepository = require("../repository/base/baseRepository");
+const Tax = require("../entities/tax");
+const Transaction = require("../entities/transaction");
+class CarService {
+  constructor({ cars }) {
+    this.carRepository = new BaseRepository({ file: cars });
+    this.taxesBasedOnAge = Tax.taxesBasedOnAge;
+    this.currencyFormat = new Intl.NumberFormat("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
   }
 
-  getRandomPositionFromArray(list){
-    const listLegnth = list.length
-    return Math.floor(
-      Math.random() * listLegnth
-    )
+  getRandomPositionFromArray(list) {
+    const listLegnth = list.length;
+    return Math.floor(Math.random() * listLegnth);
   }
 
-  chooseRandomCar(carCategory){
-    const randomCarIndex = this.getRandomPositionFromArray(carCategory.carIds)
-    const carId = carCategory.carIds[randomCarIndex]
-    return carId
+  chooseRandomCar(carCategory) {
+    const randomCarIndex = this.getRandomPositionFromArray(carCategory.carIds);
+    const carId = carCategory.carIds[randomCarIndex];
+    return carId;
   }
 
-  async givenAvaliableCar(carCategory){
-    const carId = this.chooseRandomCar(carCategory)
+  async givenAvaliableCar(carCategory) {
+    const carId = this.chooseRandomCar(carCategory);
+    
+    return this.carRepository.find(carId);
+  }
 
-    return this.carRepository.find(carId)
+  calculateFinalPrice(customer, carCategory, numberOfDays) {
+    const { age } = customer;
+    const { price } = carCategory;
+    const { then: tax } = this.taxesBasedOnAge.find(
+      (tax) => age >= tax.from && age <= tax.to
+    );
+
+    const finalPrice = ((tax*price)*(numberOfDays))
+    return this.currencyFormat.format(finalPrice)
+  }
+
+  async rent(customer, carCategory, numberOfDays) {
+    const car = await this.givenAvaliableCar(carCategory)
+
+    const finalPrice = await this.calculateFinalPrice(customer,carCategory,numberOfDays)
+
+    const today = new Date()
+    today.setDate(today.getDate() + numberOfDays)
+    const options = {
+      year:"numeric",
+      month: "long",
+      day: "numeric"
+    }
+    const dueDate = today.toLocaleDateString("pt-br",options)
+    return new Transaction({
+      customer,dueDate,car,amount:finalPrice
+    })
   }
 }
 
-module.exports = CarService
+module.exports = CarService;
--- tdd-project/test/unitTests/carService.test.js
+++ tdd-project/test/unitTests/carService.test.js
@@ -1,5 +1,7 @@
 const { describe, it, before, beforeEach, afterEach } = require("mocha");
 const CarService = require("../../src/service/carService");
+const Transaction = require("../../src/entities/transaction");
+
 const { join } = require("path");
 const { expect } = require("chai");
 const sinon = require("sinon");
@@ -36,39 +38,98 @@ describe("carService tests", () => {
     const carCategory = mocks.validCarCategory;
     const carIndex = 0;
 
-    sandbox.stub(
-      carService,
-      carService.getRandomPositionFromArray.name
-    ).returns(carIndex)
+    sandbox
+      .stub(carService, carService.getRandomPositionFromArray.name)
+      .returns(carIndex);
 
     const result = carService.chooseRandomCar(carCategory);
 
     const expected = carCategory.carIds[carIndex];
 
     expect(result).to.be.equal(expected);
-    expect(carService.getRandomPositionFromArray.calledOnce).to.be.ok
+    expect(carService.getRandomPositionFromArray.calledOnce).to.be.ok;
   });
 
   it("given a carCAtegory it should return a anvaliable car", async () => {
-    const car = mocks.validCar[0]
-    const carCategory = Object.create(mocks.validCarCategory)
+    const car = mocks.validCar[0];
+    const carCategory = Object.create(mocks.validCarCategory);
 
-    carCategory.ids = [car.id]
+    carCategory.ids = [car.id];
 
-    sandbox.stub(
-      carService.carRepository,
-      carService.carRepository.find.name
-    ).resolves(car)
+    sandbox
+      .stub(carService.carRepository, carService.carRepository.find.name)
+      .resolves(car);
 
-    sandbox.spy(
-      carService,
-      carService.chooseRandomCar.name
-    )
+    sandbox.spy(carService, carService.chooseRandomCar.name);
 
-    const carResponse = await carService.givenAvaliableCar(carCategory)
-    const expected = car
-    expect(carResponse).deep.equal(expected)
-    expect(carService.chooseRandomCar.calledOnce).to.be.ok
+    const carResponse = await carService.givenAvaliableCar(carCategory);
+    const expected = car;
+    expect(carResponse).deep.equal(expected);
+    expect(carService.chooseRandomCar.calledOnce).to.be.ok;
+  });
 
+  it("given a carCAtegory,customer and numberofdays it should calculate final amount in real", async () => {
+    const customer = Object.create(mocks.validCustomer);
+    customer.age = 50;
+
+    const carCategory = Object.create(mocks.validCarCategory);
+    carCategory.price = 37.6;
+
+    const numberOfDays = 5;
+
+    const expected = carService.currencyFormat.format(244.4);
+
+    sandbox
+      .stub(carService, "taxesBasedOnAge")
+      .get(() => [{ from: 31, to: 100, then: 1.3 }]);
+
+    const result = carService.calculateFinalPrice(
+      customer,
+      carCategory,
+      numberOfDays
+    );
+    expect(result).deep.equal(expected);
+    // expect(carService.chooseRandomCar.calledOnce).to.be.ok
+  });
+
+  it("given a carCAtegory and a car category should return transaction receipt", async () => {
+    const [car] = Object.create(mocks.validCar);
+    const customer = Object.create(mocks.validCustomer);
+
+    const carCategory = {
+      ...mocks.validCarCategory,
+      price: 37.6,
+      carIds: [car.id],
+    };
+    customer.age = 20;
+
+    sandbox
+      .stub(carService.carRepository, carService.carRepository.find.name)
+      .resolves(car);
+
+    const numberOfDays = 5;
+    const dueDate = "10 de novembro de 2020"
+
+
+    const now = new Date(2020,10,5)
+   
+
+    sandbox.useFakeTimers(now.getTime())
+    const expectedAmount = carService.currencyFormat.format(206.8)
+   
+    const expected = new Transaction({
+      customer,
+      car,
+      dueDate,
+      amount:expectedAmount
+    });
+
+    const result = await carService.rent(
+      customer,
+      carCategory,
+      numberOfDays
+    );
+
+    expect(result).deep.equal(expected);
   });
 });
